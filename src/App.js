bc4bd79d0bab844a3ceee4a2d917fc1ab9d54719
EstherWaweru <estherwaweru86@gmail.com>
2021-09-06 18:44:52 +0300

    Added th login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Login from './Login';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      
+      {/* spotify logo
+      login with spotif button */}
+    <Login/>
     </div>
   );
 }
