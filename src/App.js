c566d5465f58a41131b75bda32a12b980a23e8f7
EstherWaweru <estherwaweru86@gmail.com>
2021-09-06 19:48:12 +0300

    Using hooks to update token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import Login from './Login';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  //Run code based on given condition
+  const [token,setToken] = useState(null);
+  useEffect(() =>{
+      const hash = getTokenFromUrl();
+      window.location.hash = "";
+      const _token = hash.access_token;
+      
+      if (_token){
+        setToken(_token)
+      }
+      console.log("*********",token);
+    },[]);
+  
+  
+  
   return (
     <div className="app">
       {/* spotify logo
       login with spotif button */}
-    <Login/>
+    {token ? (
+      <h1>I am a player</h1>
+    ) : (
+      <Login/>
+    )}
+    
     </div>
   );
 }
