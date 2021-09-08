5bec1ca8d2726d8b028ec8cba5efecf40a5f6250
EstherWaweru <estherwaweru86@gmail.com>
2021-09-08 18:51:56 +0300

    Added header styling

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,21 @@
 import './Header.css'
+import { Avatar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 function Header({spotify}){
     return(
-        <div className = "header"></div>
-    )
+        <div className = "header">
+             <div className = "header__left">
+                 <SearchIcon/>
+                 <input placeholder = "Search for Artists,Songs or Podcasts"
+                 type = "text"/>
+             </div>
+         <div className = "header__right">
+            
+             <Avatar src = "" alt =""/>
+             <h4>Esther wESH</h4>
+         </div>
+        </div>
+        
+        )
 };
 export default Header;
